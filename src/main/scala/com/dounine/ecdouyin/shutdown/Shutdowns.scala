ff06098ef45dcd6cd53e6931d55c6e3b3e58ce62
lake <102535481@qq.com>
2021-05-10 17:57:08 +0800

    add shutdown and startup

---
+++ src/main/scala/com/dounine/ecdouyin/shutdown/Shutdowns.scala
@@ -0,0 +1,45 @@
+package com.dounine.ecdouyin.shutdown
+
+import akka.Done
+import akka.actor.CoordinatedShutdown
+import akka.actor.typed.ActorSystem
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import com.dounine.ecdouyin.behaviors.order.OrderBase
+import com.dounine.ecdouyin.tools.akka.db.DataSource
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+class Shutdowns(system: ActorSystem[_]) {
+  implicit val ec = system.executionContext
+  val sharding = ClusterSharding(system)
+
+  def listener(): Unit = {
+    CoordinatedShutdown(system)
+      .addTask(CoordinatedShutdown.PhaseBeforeServiceUnbind, "closeOrder") {
+        () =>
+          {
+            sharding
+              .entityRefFor(
+                OrderBase.typeKey,
+                OrderBase.typeKey.name
+              )
+              .ask(OrderBase.Shutdown()(_))(3.seconds)
+          }
+      }
+
+    CoordinatedShutdown(system)
+      .addTask(CoordinatedShutdown.PhaseBeforeServiceUnbind, "closeDb") { () =>
+        {
+          Future {
+            DataSource(system)
+              .source()
+              .db
+              .close()
+            Done
+          }
+        }
+      }
+
+  }
+
+}
---
+++ src/main/scala/com/dounine/ecdouyin/startup/Startups.scala
@@ -0,0 +1,132 @@
+package com.dounine.ecdouyin.startup
+
+import akka.Done
+import akka.actor.CoordinatedShutdown
+import akka.actor.typed.ActorSystem
+import akka.cluster.sharding.typed.ShardingEnvelope
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity}
+import akka.persistence.typed.PersistenceId
+import com.dounine.ecdouyin.behaviors.mechine.{MechineBase, MechineBehavior}
+import com.dounine.ecdouyin.behaviors.order.{OrderBase, OrderBehavior}
+import com.dounine.ecdouyin.behaviors.qrcode.QrcodeBehavior
+import com.dounine.ecdouyin.model.models.UserModel
+import com.dounine.ecdouyin.service.{OrderService, UserService}
+import com.dounine.ecdouyin.store.{
+  AkkaPersistenerJournalTable,
+  AkkaPersistenerSnapshotTable,
+  DictionaryTable,
+  OrderTable,
+  UserTable
+}
+import com.dounine.ecdouyin.tools.akka.db.DataSource
+import com.dounine.ecdouyin.tools.util.ServiceSingleton
+import org.slf4j.LoggerFactory
+import slick.lifted
+
+import java.time.LocalDateTime
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+class Startups(system: ActorSystem[_]) {
+  private val logger = LoggerFactory.getLogger(classOf[Startups])
+  implicit val ec = system.executionContext
+  val sharding = ClusterSharding(system)
+
+  def start(): Unit = {
+    sharding.init(
+      Entity(
+        typeKey = MechineBase.typeKey
+      )(
+        createBehavior = entityContext =>
+          MechineBehavior(
+            PersistenceId.of(
+              MechineBase.typeKey.name,
+              entityContext.entityId
+            )
+          )
+      )
+    )
+
+    sharding.init(
+      Entity(
+        typeKey = QrcodeBehavior.typeKey
+      )(
+        createBehavior = entityContext =>
+          QrcodeBehavior(
+            PersistenceId.of(
+              QrcodeBehavior.typeKey.name,
+              entityContext.entityId
+            )
+          )
+      )
+    )
+
+    sharding.init(
+      Entity(
+        typeKey = OrderBase.typeKey
+      )(
+        createBehavior = entityContext =>
+          OrderBehavior(
+            PersistenceId.of(
+              OrderBase.typeKey.name,
+              entityContext.entityId
+            )
+          )
+      )
+    )
+
+    ServiceSingleton.put(classOf[OrderService], new OrderService(system))
+    ServiceSingleton.put(classOf[UserService], new UserService(system))
+    import slick.jdbc.MySQLProfile.api._
+    val db = DataSource(system).source().db
+    val schemas = Seq(
+      lifted.TableQuery[UserTable].schema,
+      lifted.TableQuery[OrderTable].schema,
+      lifted.TableQuery[DictionaryTable].schema,
+      lifted.TableQuery[AkkaPersistenerJournalTable].schema,
+      lifted.TableQuery[AkkaPersistenerSnapshotTable].schema
+    )
+    schemas.foreach(schema => {
+      try {
+        Await.result(
+          db.run(schema.createIfNotExists),
+          Duration.Inf
+        )
+      } catch {
+        case e =>
+      }
+    })
+    ServiceSingleton
+      .get(classOf[UserService])
+      .add(
+        UserModel.DbInfo(
+          apiKey = "hello",
+          apiSecret = "abc",
+          balance = BigDecimal("10.00"),
+          margin = BigDecimal("0.00"),
+          createTime = LocalDateTime.now()
+        )
+      )
+      .onComplete {
+        case Failure(exception) => {
+          logger.error(exception.getMessage)
+        }
+        case Success(value) =>
+          logger.info(s"insert user apikey result ${value}")
+      }
+
+  }
+
+  def httpAfter(): Unit = {
+    sharding
+      .entityRefFor(
+        OrderBase.typeKey,
+        OrderBase.typeKey.name
+      )
+      .tell(
+        OrderBase.Run()
+      )
+  }
+
+}
